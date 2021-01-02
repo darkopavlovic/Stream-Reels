c829c7e29bda43245e1d9cd577937cd0f80c4d5f
Darko Pavlovic <44081311+darkopavlovic@users.noreply.github.com>
2021-01-01 17:41:41 -0700

    mobile ui nav fix

--- client/src/components/home/HomeNav.js
+++ client/src/components/home/HomeNav.js
@@ -1,7 +1,6 @@
 import { useContext, useEffect } from "react";
-import { AppBar, FormControl, FormControlLabel, MenuItem, Select, Switch, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Box, FormControl, FormControlLabel, MenuItem, Select, Switch, Toolbar, Typography } from "@material-ui/core";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
-import MovieFilterIcon from "@material-ui/icons/MovieFilter";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import { AppContext } from "../../context/AppContext";
 
@@ -42,22 +41,20 @@ function HomeNav() {
     <>
       <AppBar style={darkTheme ? { background: "black" } : { background: "#9146FF" }}>
         <Toolbar>
-          <MovieFilterIcon fontSize="large" style={{ paddingRight: "5px" }} />
-          <Typography variant="h5" style={{ flexGrow: 1 }}>
-            Stream Reels
-          </Typography>
           <WhatshotIcon style={{ paddingRight: "3px" }} />
           <Typography variant="h6" style={{ paddingRight: "15px" }}>
             Trending
           </Typography>
-          <FormControl style={{ paddingRight: "20px" }}>
-            <Select value={period} onChange={handleSelect} style={{ color: "white" }}>
-              <MenuItem value="day">Today</MenuItem>
-              <MenuItem value="week">Week</MenuItem>
-              <MenuItem value="month">Month</MenuItem>
-              <MenuItem value="all">All Time</MenuItem>
-            </Select>
-          </FormControl>
+          <Box style={{ flexGrow: 1 }}>
+            <FormControl style={{ paddingRight: "20px" }}>
+              <Select value={period} onChange={handleSelect} style={{ color: "white" }}>
+                <MenuItem value="day">Today</MenuItem>
+                <MenuItem value="week">Week</MenuItem>
+                <MenuItem value="month">Month</MenuItem>
+                <MenuItem value="all">All Time</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
           <FormControlLabel control={<Switch checked={darkTheme} onChange={darkMode} />} style={{ marginRight: "0px" }} />
           <Brightness4Icon style={{ marginRight: "15px" }} />
         </Toolbar>
